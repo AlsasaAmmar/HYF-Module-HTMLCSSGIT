8c62e69d84e92d61ce0942ec1462142d27d64c27
AlsasaAmmar <57010390+AlsasaAmmar@users.noreply.github.com>
2019-11-23 16:09:33 +0100

    Add files via upload

---
+++ Week 3/script.js
@@ -0,0 +1,12 @@
+function change(x) {
+     x.classList.toggle("change");
+ }
+
+  function myFunction() {
+    var y = document.getElementById("navbar");
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
+    }
+  }
